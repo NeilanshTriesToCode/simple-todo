cc9dbff4777121831150eac67d37f14c4e25822b
Neilansh Rajpal <neilanshjr@gmail.com>
2023-12-04 18:42:34 +0530

    created getters in conn.js to return db references

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,16 +1,25 @@
 // file to connect to db and other db-related methods
 const { MongoClient } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');   // to encrypt passwords
 
 const url = process.env.ATLAS_URL;
 
 // create MongoDB client to connect to the DB
 const client = new MongoClient(url);
 
+var db;        // database
+var usersDB;  // "Users" Collection containing users info
+
 // function to connect to the DB
 const connectToServer = () => {
     // connect to the server
     client.connect()
         .then(mongoClient => {
+            // initialize db and "Users" Collection
+            db = mongoClient.db('simple_todo');      // gets the "simple_todo" DB
+            usersDB = db.collection('users');        // gets the "users" Collection from the "simple_todo" DB
+
             console.log('\nConnected to the database.');
         })
         .catch(err => {
@@ -18,9 +27,19 @@ const connectToServer = () => {
         }); 
 };
 
+// getter function to return DB object
+const getDB = () => {
+    return db;
+}
+
+// getter function to return reference to the "Users" Collection
+const getUsersDB = () => {
+    return usersDB;
+}
+
 // function to signup/create user
 const signupUser = () => {
-    
+
 };
 
 // function to login user
@@ -29,8 +48,11 @@ const loginUser = () => {
 };
 
 
+
 module.exports = {
     connectToServer,
+    getDB, 
+    getUsersDB,
     signupUser, 
     loginUser,
 };
