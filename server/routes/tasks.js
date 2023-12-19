0d826c86b39b96e2fb4b7e55a4ce6682a92ed061
Neilansh Rajpal <neilanshjr@gmail.com>
2023-12-19 16:55:37 +0530

    created tasks route

---
+++ server/routes/tasks.js
@@ -0,0 +1,13 @@
+// Express route for tasks
+const express = require('express');
+
+
+// define router
+const tasksRouter = express.Router();
+
+// GET route to return all tasks of the user
+tasksRouter.get('/user/:id/tasks', async () => {
+    
+
+
+});
